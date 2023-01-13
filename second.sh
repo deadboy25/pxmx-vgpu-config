d9ae5b0fe51b357f356243319a363f23a43dec76
deadboy25 <83357729+deadboy25@users.noreply.github.com>
2023-01-13 11:21:40 -0500

    Update second.sh

--- second.sh
+++ second.sh
@@ -9,5 +9,11 @@ echo "Select Yes when asked "
 sleep 5
 /opt/vgpu_setup/NVIDIA-Linux-x86_64-510.108.03-vgpu-kvm.run --dkms
 
-sed -i '30s/include "/opt/vgpu_setup/vgpu_unlock/vgpu_unlock_hooks.c"//' /usr/src/nvidia-510.108.03/nvidia/os-interface.c
+sed -i '30s|^|#include "/opt/vgpu_setup/vgpu_unlock/vgpu_unlock_hooks.c"|' /usr/src/nvidia-510.108.03/nvidia/os-interface.c
+echo ldflags-y += -T /opt/vgpu_setup/vgpu_unlock/kern.ld >> /usr/src/nvidia-510.108.03/nvidia/kern.ld
 
+cd /opt/vgpu_setup/vgpu_unlock-rs
+cargo build --release
+echo REBOOTING IN 5
+sleep 5
+reboot

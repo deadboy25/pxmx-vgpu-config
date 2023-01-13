e58b370c1c3a590bc90d79a347c7b51e998864c6
deadboy25 <83357729+deadboy25@users.noreply.github.com>
2023-01-13 10:47:08 -0500

    Update second.sh

--- second.sh
+++ second.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
 # enter working directory
-cd /opt/vgpu_setup
+#cd /opt/vgpu_setup
 
 # make host driver executable and install as kernel module
-chmod +x NVIDIA-Linux-x86_64-510.108.03-bgpu-kvm.run #TODO: test
+chmod +x /opt/vgpu_setup/NVIDIA-Linux-x86_64-510.108.03-bgpu-kvm.run #TODO: test
 echo "Select Yes when asked "
 sleep 5
-./NVIDIA-Linux-x86_64-510.108.03-bgpu-kvm.run --dkms
+./opt/vgpu_setup/NVIDIA-Linux-x86_64-510.108.03-bgpu-kvm.run --dkms
